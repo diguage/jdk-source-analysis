ad98f0800afd4cbcd26a6cea9a1fcab678b4b87a
diguage <leejun119@gmail.com>
2020-03-09 15:54:12 +0800

    add new content

---
+++ src/main/java/com/diguage/truman/ConcurrentSkipListMapTest.java
@@ -0,0 +1,19 @@
+package com.diguage.truman;
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2020-03-08 16:43
+ */
+public class ConcurrentSkipListMapTest {
+    @Test
+    public void test() {
+        ConcurrentSkipListMap<Integer, Integer> map = new ConcurrentSkipListMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put(i, i);
+        }
+    }
+}
